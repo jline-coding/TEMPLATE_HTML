ee508a1bb4fdcec35ca83c95b7e16ca752a88956
hiep011094 <hiep011094@gmail.com>
2026-05-07 08:36:00 +0700

    fix all

--- src__php/components/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="c-footer">
-    <div class="l_container">
-        <div class="c-footer__inner">
-            
-            <p class="c-footer__copy">©</p>
-        </div>
-    </div>
-    <a href="#wrapper" class="c-totop">PAGETOP</a>
-</footer>
